eff14f4c8f1cfe5a5303cdb012ce813664d787a4
Razvan-Ionut Mititelu <Razvan-Ionut Mititelu>
2016-04-10 20:30:20 +0300

    First Commit

---
+++ FirstTryInno/FirstTryInno/View/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using FirstTryInno.ViewModel;
+using System;
+using Windows.Devices.Gpio;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FirstTryInno
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private MainPageViewModel _viewModel; 
+
+        public MainPage()
+        {
+            InitializeComponent();
+            InitializeCustomComponents();
+         }
+               
+        private void InitializeCustomComponents()
+        {
+            _viewModel = new MainPageViewModel();
+            _viewModel.Changed += _viewModel_Changed;
+            
+        }
+
+        private void _viewModel_Changed(object sender, EventArgs e)
+        {
+            // need to invoke UI updates on the UI thread because this event
+            // handler gets invoked on a separate thread.
+            var task = Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => 
+            {
+                labelName.Text = _viewModel.CustomText;
+            });
+            
+        }
+    }
+}
---
+++ FirstTryInno/FirstTryInno/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Windows.Devices.Gpio;
+using Windows.UI.Xaml;
+using FirstTryInno.Helpers;
+
+namespace FirstTryInno.ViewModel
+{
+    public class MainPageViewModel 
+    {
+
+#region Private Members 
+
+        private GpioPin _buttonLike;
+        private GpioPin _buttonDisLike;
+        private GpioPin _buttonUp;
+        private GpioPin _buttonDown;
+        private GpioController _ioController;
+        private string _customText;
+
+#endregion
+
+        public event EventHandler Changed;
+        public string CustomText
+        {
+            get
+            {
+                return _customText;
+            }
+            set
+            {
+                _customText = value;
+                OnChanged(EventArgs.Empty);
+            }
+        }
+
+        public MainPageViewModel()
+        {
+            InitGpio();
+        }
+        
+        private void InitGpio()
+        {
+            _ioController = GpioController.GetDefault();
+            if (_ioController == null)
+            {
+                CustomText = "GPIO failed...";
+                return;
+            }
+
+            InitButtons();
+        }
+
+        private void InitButtons()
+        {
+            _buttonUp = _ioController.OpenPin(GPIOConstants.BUTTON_UP_PIN);
+            _buttonDown = _ioController.OpenPin(GPIOConstants.BUTTON_DOWN_PIN);
+            _buttonLike = _ioController.OpenPin(GPIOConstants.BUTTON_LIKE_PIN);
+            _buttonDisLike = _ioController.OpenPin(GPIOConstants.BUTTON_DISLIKE_PIN);
+
+            SetButtonBehaviour(_buttonUp);
+            SetButtonBehaviour(_buttonDown);
+            SetButtonBehaviour(_buttonLike);
+            SetButtonBehaviour(_buttonDisLike);
+
+            // Connect them to thei handlers.
+            _buttonUp.ValueChanged += _buttonUp_ValueChanged;
+            _buttonDown.ValueChanged += _buttonDown_ValueChanged;
+            _buttonLike.ValueChanged += _buttonLike_ValueChanged;
+            _buttonDisLike.ValueChanged += _buttonDisLike_ValueChanged;
+        }
+
+        private static void SetButtonBehaviour(GpioPin btn)
+        {
+            if(btn == null)
+                return;
+            
+            // Check if input pull-up resistors are supported
+            if (btn.IsDriveModeSupported(GpioPinDriveMode.InputPullUp))
+                btn.SetDriveMode(GpioPinDriveMode.InputPullUp);
+            else
+                btn.SetDriveMode(GpioPinDriveMode.Input);
+
+            // Set a debounce timeout to filter out switch bounce noise from a button press
+            btn.DebounceTimeout = TimeSpan.FromMilliseconds(50);
+        }
+
+#region Event Handlers
+
+        private void _buttonUp_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
+        {
+            if (args.Edge == GpioPinEdge.FallingEdge)
+            {
+                CustomText = "Button Up was pressed...";
+            }
+        }
+
+        private void _buttonDown_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
+        {
+            if (args.Edge == GpioPinEdge.FallingEdge)
+            {
+                CustomText = "Button Down was pressed...";
+            }
+        }
+
+        private void _buttonLike_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
+        {
+            if (args.Edge == GpioPinEdge.FallingEdge)
+            {
+                CustomText = "Button Like was pressed...";
+            }
+        }
+
+        private void _buttonDisLike_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
+        {
+            if (args.Edge == GpioPinEdge.FallingEdge)
+            {
+                CustomText = "Button DisLike was pressed...";
+            }
+        }
+
+#endregion
+
+        // Invoke the Changed event; called whenever list changes:
+        protected virtual void OnChanged(EventArgs e)
+        {
+            Changed?.Invoke(this, e);
+        }
+    }
+}
